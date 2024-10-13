efa92a11899c153735a260e15cc3e77a45706cf0
cquispe <claudialibertad05@gmail.com>
2024-10-13 13:41:16 -0500

    conexion bd correcta

--- bd.py
+++ bd.py
@@ -2,13 +2,23 @@ import pymysql
 from dotenv import load_dotenv
 import os
 
+with open('.env') as f:
+    print(f.read())
+
 # Cargar las variables de entorno desde el archivo .env
-load_dotenv()
+load_dotenv(dotenv_path='.env')
 
 def obtener_conexion():
+    # Imprimir las variables de entorno para verificar su contenido
+    print("DB_HOST:", os.getenv('DB_HOST'))
+    print("DB_PORT:", os.getenv('DB_PORT'))
+    print("DB_USER:", os.getenv('DB_USER'))
+    print("DB_PASSWORD:", os.getenv('DB_PASSWORD'))
+    print("DB_NAME:", os.getenv('DB_NAME'))
+
     return pymysql.connect(
         host=os.getenv('DB_HOST'),         # Leer host desde .env
-        port=int(os.getenv('DB_PORT')),    # Leer puerto desde .env
+        port=int(os.getenv('DB_PORT', 3306)),  # Establecer un valor por defecto
         user=os.getenv('DB_USER'),         # Leer usuario desde .env
         password=os.getenv('DB_PASSWORD'), # Leer contraseña desde .env
         db=os.getenv('DB_NAME')            # Leer base de datos desde .env
