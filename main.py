c112efe2db7ab5c82559c3af34d23daf9656400a
cquispe <claudialibertad05@gmail.com>
2024-10-13 13:23:18 -0500

    conexion_bd corregida

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, jsonify
-
+import controlador_producto
 
 app = Flask(__name__)
 
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.route("/amefil")
 def amefil():
     return render_template("Index.html")
+
+
+@app.route("/navegacionproductos")
+def navegacionproductos():
+    productos_con_imagen = controlador_producto.obtener_productos()
+    return render_template("Navegacion_productos.html", productos_con_imagen = productos_con_imagen)
 
 # Iniciar el servidor
 if __name__ == "__main__":
