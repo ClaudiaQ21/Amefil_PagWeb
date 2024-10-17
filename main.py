21d9003e50dc6143871c7e505aff6d11c97b9574
cquispe <claudialibertad05@gmail.com>
2024-10-17 11:26:17 -0500

    cq7_main ruta alternativa A

--- main.py
+++ main.py
@@ -207,6 +207,10 @@ def productoadmin():
 def usuarioadmin():
     return render_template("UsuarioLME.html")
 
+@app.route("/agregar_rol")
+def formulario_agregar_rol():
+    return render_template("Agregar_Rol.html")
+
 # Iniciar el servidor
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
