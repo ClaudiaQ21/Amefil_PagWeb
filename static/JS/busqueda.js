6bfb850378ab0336a84a6511b2f465ac86736f83
anny <cotos.sheyla@gmail.com>
2024-10-20 18:56:53 -0500

    AC_Busqueda

--- static/JS/busqueda.js
+++ static/JS/busqueda.js
@@ -1,7 +1,23 @@
 function buscarProducto(event) {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
         event.preventDefault();
-        var busqueda = document.getElementById('busqueda').value;
-        window.location.href = 'Navegación productos.html?busqueda=' + encodeURIComponent(busqueda);
+
+        // Determinar qué campo de búsqueda se está usando
+        let query = "";
+        const inputMovil = document.getElementById("busqueda-movil");
+        const inputGrande = document.getElementById("busqueda-grande");
+
+        if (inputMovil && inputMovil.offsetParent !== null) {
+            // Si el campo de búsqueda móvil está visible
+            query = inputMovil.value;
+        } else if (inputGrande && inputGrande.offsetParent !== null) {
+            // Si el campo de búsqueda para pantallas más grandes está visible
+            query = inputGrande.value;
+        }
+
+        if (query) {
+            // Redirigir con la consulta de búsqueda
+            window.location.href = `/navegacionproductos?busqueda=${encodeURIComponent(query)}`;
+        }
     }
 }
