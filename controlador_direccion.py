a05180c600ea084714516b296bb60cccd8e53833
cquispe <claudialibertad05@gmail.com>
2024-10-20 20:56:34 -0500

    CQ_20_10_CorreccionJSAgain

--- controlador_direccion.py
+++ controlador_direccion.py
@@ -14,7 +14,7 @@ def obtener_direcciones():
 def obtener_direccion_completa():
     conexion = obtener_conexion()
     with conexion.cursor() as cursor:
-        cursor.execute("select dir.id_direccion, dir.detalle, ubi.departamento, ubi.provincia, ubi.distrito from direccion dir inner join ubigeo ubi on dir.id_ubigeo = ubi.id_ubigeo")
+        cursor.execute("select id_direccion from direccion")
         direcciones = cursor.fetchall()
     conexion.close()
     return direcciones
